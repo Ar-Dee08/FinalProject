92d34478c9df46dafe88364b9337b6875f4001cc
RD <rdeeflores08@gmail.com>
2024-12-05 17:13:19 +0800

    kwaza kwaza

--- adminside/homeadmin.php
+++ adminside/homeadmin.php
@@ -14,12 +14,6 @@ include 'includes/header.php';
 <body class="logo-bg-2">
     <div>
         <h1>Home</h1>
-    <!-- Sidebar JS -->
-    <div>
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
-    </div>
-
     <?php
         include 'includes/footer.php';
     ?>
