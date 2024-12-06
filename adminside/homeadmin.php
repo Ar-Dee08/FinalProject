2d6da958d37b5438cfcd73e6ec9d0f7159ecc017
RD <rdeeflores08@gmail.com>
2024-12-06 23:04:51 +0800

    test

--- adminside/homeadmin.php
+++ adminside/homeadmin.php
@@ -2,7 +2,6 @@
 // session_start();
 include 'includes/header.php';
 include 'admin_middleware.php';
-include 'includes/footer.php';
 include '../vscode/dbcon.php';
 ?>
 
@@ -15,7 +14,6 @@ include '../vscode/dbcon.php';
 </head>
 <body class="logo-bg-2">
     <div class="home-txt">
-    
     
     <?php
         if (isset($_SESSION['admin_id'])) {
@@ -44,35 +42,9 @@ include '../vscode/dbcon.php';
                 echo '<p style="color: #458D9E;" class="error-login" align="center">' . $_GET['error'] . '</p>';
             }            
         ?> 
-    <div>
-    <div class="list-group">
-                <a href="view_category.php" class="list-group-item list-group-item-action">CATEGORIES</a>
-                <a href="view_product.php" class="list-group-item list-group-item-action">PRODUCTS/ITEMS</a>
-                <a href="view_news.php" class="list-group-item list-group-item-action">NEWS & UPDATES</a>
-                <a href="view_admin.php" class="list-group-item list-group-item-action">ADMINISTRATORS</a>
-                <a href="#" class="list-group-item list-group-item-action">TRANSACTIONS</a>
-                <?php 
 
-if(isset($_SESSION['isPriv'])){ ?>
-    <a class="list-group-item list-group-item-action" 
-    data-bs-toggle="collapse" 
-    href="#collapseAccounts" 
-    role="button" 
-    aria-expanded="false" 
-    aria-controls="collapseAccounts">
-        USER ACCOUNT RECORDS
-    </a>
-    <div class="collapse" id="collapseAccounts">
-        <ul class="list-group mt-2">
-            <a class="list-group-item list-group-item-action" href="view_userinfo.php">USER INFORMATION</a>
-            <a class="list-group-item list-group-item-action" href="view_useracc.php">USER ACCOUNT DETAILS</a>
-        </ul>
-<?php }
-
-?>
-
-
-                
-                </div>
-    </div>
-    </div>
+<div class="footer-footer">
+    <?php
+        include 'includes/footer.php';
+    ?>
+</div>
